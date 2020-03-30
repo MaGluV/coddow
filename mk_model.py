83f66e6ac7c3bc14e65eb6fd885f36d6ee03034e
max <petrow-wodcin@yandex.ru>
2020-03-30 19:01:26 +0300

    Add GPL License information

--- mk_model.py
+++ mk_model.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# -*- Mode: Python; coding: utf-8; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- 
+
+# comdb.py
+#
+# Copyright (C) 2020 - max
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 from downcom import Download 
 from readconf import ReadConfig
